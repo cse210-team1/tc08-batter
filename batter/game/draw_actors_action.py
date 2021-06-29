6250c3627879a444b1f82fb2263b672fefb9fb82
David Bloomfield <david.s.bloomfield@gmail.com>
2021-06-29 15:32:18 -0600

    added the needed files

---
+++ batter/game/draw_actors_action.py
@@ -0,0 +1,16 @@
+from game.action import Action
+
+class DrawActorsAction(Action):
+
+	def __init__(self, output_service):
+		
+		self._output_service = output_service
+		
+	def execute(self, cast):
+		
+		self._output_service.clear_screen()
+		for tag in cast:
+			self._output_service.draw_actors(cast[tag])
+		self._output_service.flush_buffer()
+		
+		
