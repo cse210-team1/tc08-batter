b587afdaffb825c9a330cf00735ee3b0656d7a71
Jacob Cloward <jacobcloward@yahoo.com>
2021-06-29 15:49:00 -0600

    updated main and constants

--- batter/game/constants.py
+++ batter/game/constants.py
@@ -1,8 +1,3 @@
-import os
-
 MAX_X = 120
 MAX_Y = 40
 FRAME_LENGTH = 0.1
-PATH = os.path.dirname(os.path.abspath(__file__))
-MESSAGES = open(PATH + "/messages.txt").read().splitlines()
-ARTIFACTS = 30
