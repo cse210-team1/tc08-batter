0cd2752aad9963db2cba28b7a5b5367ecbc8cd52
David Bloomfield <david.s.bloomfield@gmail.com>
2021-06-29 16:02:24 -0600

    my ideas

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -1,6 +1,8 @@
 import random
 from game import constants
 from game.action import Action
+from game.point import Point
+import sys
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
@@ -15,11 +17,56 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        marquee = cast["marquee"][0] # there's only one
-        robot = cast["robot"][0] # there's only one
-        artifacts = cast["artifact"]
-        marquee.set_text("")
-        for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()):
-                description = artifact.get_description()
-                marquee.set_text(description) 
+        ball = cast["ball"][0] # there's only one
+        paddle = cast["paddle"][0] # there's only one
+        bricks = cast["brick"]
+        self._check_bricks(bricks, ball)
+        self._check_walls(ball)
+        self._check_paddle(paddle, ball)
+                
+    def _check_bricks(self, bricks, ball):
+        for brick in bricks:
+            if ball.get_position().equals(brick.get_position()):
+                self._bounce(ball,"y")
+                bricks.remove(brick)
+    
+
+    def _bounce(self, ball, direction):
+        """ Tells the ball what to do when it hits an object
+        
+        Args: 
+            ball (Actor): the ball that needs to bounce
+            direction (String): x or y, what direction is the bounce occuring"""
+        velocity = ball.get_velocity()
+        x = velocity.get_x()
+        y = velocity.get_y()
+        
+        if direction == "x":
+            x *= -1
+        elif direction == "y":
+            y *= -1
+        ball.set_velocity(Point(x,y))
+
+
+    def _check_walls(self, ball):
+        x = ball.get_position().get_x()
+        y = ball.get_position().get_y()
+        if (x == 1) or (x == constants.MAX_X):
+            self._bounce(ball,"x")
+        if (y == 0):
+            self._bounce(ball,"y")
+        if (y == constants.MAX_Y):
+            sys.exit()
+            
+    def _check_paddle(self,paddle, ball):
+        ball_position = ball.get_position()
+        bat_y = paddle.get_position().get_y()
+        bat_x = paddle.get_position().get_x()
+        for i in range(len(paddle.get_text())):
+            x = bat_x + i
+            if ball_position.equals(Point(x,bat_y)):
+                self._bounce(ball,"y")
+        
+        
+    
+        
